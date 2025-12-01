92b8c9abba802d7d7bd2240551ec20804cdc6a45
ytpo-arp <77130684+ytpo-arp@users.noreply.github.com>
2025-12-01 12:16:09 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-// Root directory for static files (one level up from Render's /src/)
-const rootDir = path.resolve(__dirname, "..");
+// Root directory for static files (Render clone root)
+const rootDir = __dirname;
 console.log("Serving static files from:", path.join(rootDir, "public"));
 
 // Serve all frontend files from /public
