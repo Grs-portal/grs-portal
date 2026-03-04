0127f860db0e444db7b61a9a362c7886afb24422
ytpo-arp <77130684+ytpo-arp@users.noreply.github.com>
2026-03-03 22:21:40 -0400

    Update server.js

--- server.js
+++ server.js
@@ -49,11 +49,9 @@ const DATA_FILE = path.join(__dirname, "data.json");
 function defaultData() {
   return {
     accounts: [
-      accounts: [
   { username: "root", password: "1234", role: "instructor", name: "Instructor Root", email: "", avatarUrl: "" },
   { username: "manager", password: "9999", role: "manager", name: "Project Manager", email: "", avatarUrl: "" },
   { username: "student", password: "1234", role: "student", name: "Student", email: "", avatarUrl: "" },
-      ],
     ],
     courses: [{ id: 1, title: "Intro to Programming", description: "Learn JS basics", locationType: "in-person" }],
     homework: [
@@ -897,5 +895,6 @@ app.get("/homepage/register.html", (req, res) => sendFirstExisting(res, "homepag
 
 // ---------------- START ----------------
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+
 
 
