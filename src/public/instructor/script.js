d7b60fd437c2448bcd0d5dcd607b4c7788743d27
CodeCat <luviennastjago25@gmail.com>
2026-03-14 00:54:31 -0400

    Update script.js

--- src/public/instructor/script.js
+++ src/public/instructor/script.js
@@ -1,4 +1,4 @@
-// manager.js
+// teacher.js
 (() => {
   const API = "/api";
   const LOGIN = "/homepage/login-instructor.html";
@@ -45,7 +45,7 @@
 
   async function init() {
     if (localStorage.getItem("isLoggedIn") !== "true") return (location.href = LOGIN);
-    if (localStorage.getItem("role") !== "teacher") return (location.href = LOGIN);
+    if (localStorage.getItem("role") !== "instructor") return (location.href = LOGIN);
 
     qs("#y").textContent = new Date().getFullYear();
 
